ca801d4e95e33e8f938506a58eab8e7fe5c0ab9c
somunelavalli <47572851+somunelavalli@users.noreply.github.com>
2021-12-17 14:17:45 +0530

    Extra comma removed from admin pages
    Extra comma removed from admin pages

--- client/src/pages/AdminHome.js
+++ client/src/pages/AdminHome.js
@@ -66,7 +66,6 @@ function AdminHome() {
                         style={{ color: "red", cursor: "pointer" }}
                       />
                     </Popconfirm>
-                    ,
                   </div>
                 </div>
               </div>
